d4878070a35f5edf62dd10b9019f9e8f78c1784c
Carl Björknäs <carl.bjorknas@knowit.se>
2018-09-14 11:00:41 +0200

    Adds error handling to product resolving function.

--- APM-Start/src/app/products/product-resolver.service.ts
+++ APM-Start/src/app/products/product-resolver.service.ts
@@ -1,5 +1,5 @@
 import { IProduct } from "./product";
-import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from "@angular/router";
+import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot, Router } from "@angular/router";
 import { Observable } from "rxjs/Observable";
 import { Injectable } from "@angular/core";
 import { ProductService } from "./product.service";
@@ -8,10 +8,31 @@ import { ProductService } from "./product.service";
 
 export class ProductResolver implements Resolve<IProduct>{
 
-    constructor(private productService: ProductService){}
+    constructor(
+        private productService: ProductService,
+        private router: Router ){}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IProduct> {
-        let id = +route.paramMap.get('id')
-        return this.productService.getProduct(id)
+        let id = route.paramMap.get('id')
+        if (isNaN(+id)){
+            console.log(`Product id was not a number: ${id}`)
+            this.router.navigate(['/products'])
+            return Observable.of(null)
+        }
+
+        return this.productService.getProduct(+id)
+            .map(product => {
+                if (product) {
+                    return product
+                }
+                console.log(`Product was not found: ${id}`)
+                this.router.navigate(['/products'])
+                return null
+            })
+            .catch(error => {
+                console.log(`Retrieval error: ${error}`)
+                this.router.navigate(['/products'])
+                return Observable.of(null)
+            })
     }
 }
